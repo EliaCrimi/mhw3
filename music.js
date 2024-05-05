18695b3c3f20ba3399251ae00707ffbe617654db
EliaCrimi <163771500+EliaCrimi@users.noreply.github.com>
2024-05-06 00:22:32 +0200

    Update music.js

--- music.js
+++ music.js
@@ -3,30 +3,23 @@ let token;
 const client_id = "4f9ea3998d244005ae4b56289da1ac4f";
 const client_secret = "f1037da3ce7b4434ba73b91a07cbd7f7";
 
-function getArtistUUID(artistName) {
-    const artistUUIDs = {
-        "pitbull": "0TnOYISbd1XYRBk9myaseg",
-        "theweeknd": "1Xyo4u8uXC1ZmMpatF05PJ",
-        "bts": "3Nrfpe0tUJi4K4DXYWgMUX",
-    };
-
-    const lowercaseName = artistName.toLowerCase();
-    return artistUUIDs[lowercaseName];
-}
 
 function onJson(json) {
     console.log(json);
 
-    const images = json.images;
+    const artistItems = json.artists.items;
+    const artist = artistItems[0]; 
+    const images = artist.images;
     const urlimg = images[0].url;
     console.log("URL: ", urlimg);
-
-    const artist = document.querySelector('#image-container');
-    const ImmagineArtista = document.createElement('img');
-    ImmagineArtista.src = urlimg;
-    artist.appendChild(ImmagineArtista);
-    ImmagineArtista.classList.add('ArtistaImmagine');
+    const artistContainer = document.querySelector('#image-container');
+    const artistImage = document.createElement('img');
+    artistImage.src = urlimg;
+    artistContainer.appendChild(artistImage);
+    artistImage.classList.add('ArtistaImmagine');
+    
 }
+
 
 function onResponse(response) {
     console.log('Response received');
@@ -40,16 +33,16 @@ function search(event) {
     const artist_value = encodeURIComponent(artist_input.value);
     console.log('Searching for: ' + artist_value);
 
-    const artist_uuid = getArtistUUID(artist_value);
-    const rest_url = 'https://api.spotify.com/v1/artists/' + artist_uuid;
-
-    fetch(rest_url, {
+    const apiUrl = `https://api.spotify.com/v1/search?q=${artist_value}&type=artist`;
+    fetch(apiUrl, {
         headers: {
             'Authorization': 'Bearer ' + token
         }
     }).then(onResponse)
         .then(onJson);
 }
+
+
 
 function onTokenJson(json) {
     token = json.access_token;
